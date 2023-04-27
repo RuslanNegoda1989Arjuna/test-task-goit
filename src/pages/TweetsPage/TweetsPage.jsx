172451e206e7fb6e6dfd392921f73b6941b754bf
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-28 00:43:18 +0300

    first part

--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -1,12 +1,21 @@
 import HeaderImage from "../../shared/components/HeaderImage/HeaderImage"
 import Logo from "../../shared/components/Logo/Logo"
+import styles from './tweets-page.module.scss'
+import Avatar from '../../images/Boy.png';
 
 const TweetsPage = () => {
     return (
         <div className="container">
             <div className="mainBox">
                 <Logo />
-                <HeaderImage/>
+                <HeaderImage />
+                <img className={styles.avatarka} src={Avatar} alt="Avatarka" />
+                <p className={styles.tweets}>777 tweets</p>
+                <p className={styles.followers}>100,500 follows</p>
+                <div className={styles.btnWrapper}>
+                    <button className={styles.button}>FOLLOW</button>
+                </div>
+                
             </div>
            
 
