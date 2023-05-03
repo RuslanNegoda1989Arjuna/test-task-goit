0c29bfc1a2b8513f3505c86bea247bf27fdf4a14
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-05-03 22:32:08 +0300

    fix redirect to main page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './shared/styles/styles.scss'
 import HomePage from './pages/HomePage/HomePage';
 import TweetsPage from './pages/TweetsPage/TweetsPage'
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='tweets' element={<TweetsPage/>}/>
-        <Route path="*" element={<HomePage/> } />
+        <Route path="*" element={<Navigate to='/'/>} />
       </Routes>
     </BrowserRouter>
    
